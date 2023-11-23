00915358a93108e76cd50747bcba03408c7d07b6
Syed-Huzaifa1 <shahhuzaifa338@gmail.com>
2023-11-23 15:27:55 +0500

    add more data filters

--- app/Filters/V1/InvoicesFilter.php
+++ app/Filters/V1/InvoicesFilter.php
@@ -5,19 +5,19 @@ namespace App\Filters\V1;
 use Illuminate\Http\Request;
 use App\Filters\ApiFilter;
 
-class CustomersFilter extends ApiFilter {
+class InvoicesFilter extends ApiFilter {
   protected $safeParms = [
-    'name' => ['eq'],
-    'type' => ['eq'],
-    'email' => ['eq'],
-    'address' => ['eq'],
-    'city' => ['eq'],
-    'state' => ['eq'],
-    'postalCode' => ['eq', 'gt', 'lt']
+    'customerId' => ['eq'],
+    'amount' => ['eq', 'lt', 'gt', 'lte', 'gte'],
+    'status' => ['eq', 'ne'],
+    'billedDate' => ['eq', 'lt', 'gt', 'lte', 'gte'],
+    'paidDate' => ['eq', 'lt', 'gt', 'lte', 'gte'],
   ];
 
   protected $columnMap = [
-    'postalCode' => 'postal_code'
+    'customerId' => 'customer_id',
+    'billedDate' => 'billed_date',
+    'paidDate' => 'paid_date',
   ];
 
   protected $operatorMap = [
@@ -26,27 +26,6 @@ class CustomersFilter extends ApiFilter {
     'lte' => '<=',
     'gt' => '>',
     'gte' => '>=',
+    'ne' => '!='
   ];
-
-  public function transform(Request $request) {
-    $eloQuery = [];
-
-    foreach ($this->safeParms as $parm => $operators) {
-      $query = $request->query($parm);
-
-      if (!isset($query)) {
-        continue;
-      }
-
-      $column = $this->columnMap[$parm] ?? $parm;
-
-      foreach ($operators as $operator) {
-        if (isset($query[$operator])) {
-          $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
-        }
-      }
-    }
-
-    return $eloQuery;
-  }
 }
